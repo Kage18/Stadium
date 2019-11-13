6789fadf1ac6e5e5ecfdb46b95da33fc654261a2
Harshit Singh <singh.harshit262@gmail.com>
2019-11-13 22:50:58 +0530

    minor fixes

--- Stadium/Stadium/settings.py
+++ Stadium/Stadium/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_ROOT = BASE_DIR + 'media/'
+MEDIA_ROOT = BASE_DIR + '/media/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'users',
     'merch',
     'library',
+    'transactions',
 ]
 
 
